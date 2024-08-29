0da6c833d039c7c00639dbc367df683d0cab0ac8
huzipatel <huzaifa@live.co.uk>
2024-08-29 20:33:55 +0100

    add Dockerfile

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ INFLUXDB_URL = "http://influxdb-service.monitoring.svc.cluster.local:8086"
 INFLUXDB_TOKEN = "sL0JLLOmld1bN1kBWpWx3RZ3Scd6ZeOOYVbWkeKRPkGqKI7-0sehEzH05Xlm9r6zeqvYyCH0DmalpYlgOCfJxA=="
 INFLUXDB_ORG = "HuziPersonal"
 INFLUXDB_BUCKET = "WeatherAPI"
-API_URL = "<REALTIME-AIR-QUALITY-API-URL>"
-API_KEY = "<YOUR-API-KEY>"
+API_URL = "https://api.marketstack.com/v1/eod?symbols=AAPL"
+API_KEY = "18d39a0626eaf7cdd4a23b2e9c962150"
 
 client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 write_api = client.write_api(write_options=WriteOptions(batch_size=1))
 
-def fetch_air_quality_data():
-    response = requests.get(API_URL, headers={"apikey": API_KEY})
+def fetch_end_of_day_data():
+    response = requests.get(API_URL, API_KEY)
     data = response.json()
     return data
+    print(data)
+
+
+
 
 def process_and_store_data(data):
     # Example: Suppose data contains "pm2.5" and "pm10" values
