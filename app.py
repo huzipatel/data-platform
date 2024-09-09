ce647f3da98f4cf09391ab5acd52e4bdd2e521ed
huzipatel <huzaifa@live.co.uk>
2024-09-09 23:11:38 +0100

    Increment offset by limit

--- app.py
+++ app.py
@@ -18,18 +18,20 @@ API_KEY = os.getenv('API_KEY')
 client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 write_api = client.write_api(write_options=WriteOptions(batch_size=1))
 
+offset = 0
+limit = 100
+
+
 
 def fetch_end_of_day_data():
     # Create a loop to fetch all pages
     while True:
-        offset = 0
-        limit = 100
         # Define the parameters, including the offset and limit
         params = {
     'access_key': API_KEY,
     'symbols': 'AAPL',
-    'offset':offset,
-    'limit':limit
+    'offset': offset,
+    'limit': limit
 }
         #Make the GET request
         response = requests.get(API_URL, params = params)
@@ -41,16 +43,10 @@ def fetch_end_of_day_data():
 
                 #Processing code here
                 print(item)
-         
-        #C
-        #if data.get()
-        print(response)
         
-        data = response.json()
+        offset += limit
         
-        print(data)
         
-        return data
     
 
 
